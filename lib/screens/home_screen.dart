4fa37c2c9991de7a4739ec7455ebe9cb0a42bf2e
wanakiki <1366026816@qq.com>
2023-02-28 21:23:52 +0800

    Implement hotel screen widget.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:booktickets/screens/hotel_screen.dart';
 import 'package:booktickets/screens/ticket_view.dart';
 import 'package:booktickets/utils/app_styles.dart';
 import 'package:flutter/material.dart';
@@ -28,25 +29,30 @@ class _HomeScreenState extends State<HomeScreen> {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Good morning", style: Styles.headLineStyle4,),
-                        Text("Book Tickets", style: Styles.headLineStyle1,)
+                        Text(
+                          "Good morning",
+                          style: Styles.headLineStyle4,
+                        ),
+                        Text(
+                          "Book Tickets",
+                          style: Styles.headLineStyle1,
+                        )
                       ],
                     ),
                     Container(
                       height: 50,
                       width: 50,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: const DecorationImage(
-                          image: AssetImage("assets/images/image1.png")
-                        )
-                      ),
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              image: AssetImage("assets/images/image1.png"))),
                     )
                   ],
                 ),
                 const Gap(25),
                 Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(5),
@@ -54,20 +60,29 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Row(
                     children: [
                       Icon(Icons.search, color: primary),
-                      Text("Search", style: Styles.headLineStyle4,)
+                      Text(
+                        "Search",
+                        style: Styles.headLineStyle4,
+                      )
                     ],
                   ),
                 ),
                 const Gap(40),
                 Row(
                   children: [
-                    Text("Upcoming Flights", style: Styles.headLineStyle2,),
+                    Text(
+                      "Upcoming Flights",
+                      style: Styles.headLineStyle2,
+                    ),
                     const Spacer(),
                     InkWell(
-                      onTap: (){
-                        print("View all clicked");
-                      },
-                        child: Text("View all", style: Styles.headLineStyle4.copyWith(color: primary),)),
+                        onTap: () {
+                          print("View all clicked");
+                        },
+                        child: Text(
+                          "View all",
+                          style: Styles.headLineStyle4.copyWith(color: primary),
+                        )),
                   ],
                 )
               ],
@@ -78,12 +93,43 @@ class _HomeScreenState extends State<HomeScreen> {
             scrollDirection: Axis.horizontal,
             padding: const EdgeInsets.only(left: 20),
             child: Row(
-              children: [
+              children: const [
                 TicktView(),
                 TicktView(),
               ],
             ),
-          )
+          ),
+          const Gap(15),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 17),
+            child: Row(
+              children: [
+                Text(
+                  "Upcoming Flights",
+                  style: Styles.headLineStyle2,
+                ),
+                const Spacer(),
+                InkWell(
+                    onTap: () {
+                      print("View all clicked");
+                    },
+                    child: Text(
+                      "View all",
+                      style: Styles.headLineStyle4.copyWith(color: primary),
+                    )),
+              ],
+            ),
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left: 20),
+              child: Row(
+                children: [
+                  HotelScreen(),
+                  HotelScreen(),
+                ],
+              )),
         ],
       ),
     );
